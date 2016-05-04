6fba2b3cf81bfe76747c52f511669a517952c3ad
Adiss <adiss.b17@gmail.com>
2016-05-04 20:31:48 +0200

    Some improvements added to the BattleScene

--- src/main/java/hu/experiment_team/controllers/FxmlBattleSceneController.java
+++ src/main/java/hu/experiment_team/controllers/FxmlBattleSceneController.java
@@ -7,10 +7,12 @@ import hu.experiment_team.models.Pokemon;
 import hu.experiment_team.models.Trainer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 import java.net.URL;
@@ -25,7 +27,7 @@ public class FxmlBattleSceneController implements Initializable {
     @FXML
     public AnchorPane top_battle_scene_holder;
     @FXML
-    public GridPane OpponentPokemonStatusPanel;
+    public AnchorPane OpponentPokemonStatusPanel;
     @FXML
     public Text OpponentPokemonName;
     @FXML
@@ -33,7 +35,7 @@ public class FxmlBattleSceneController implements Initializable {
     @FXML
     public Text OpponentPokemonHpText;
     @FXML
-    public GridPane MyPokemonStatusPanel;
+    public AnchorPane MyPokemonStatusPanel;
     @FXML
     public Text MyPokemonName;
     @FXML
@@ -44,6 +46,14 @@ public class FxmlBattleSceneController implements Initializable {
     public ImageView myPokemonImage;
     @FXML
     public ImageView opponentPokemonImage;
+    @FXML
+    public Button Move1Button;
+    @FXML
+    public Button Move2Button;
+    @FXML
+    public Button Move3Button;
+    @FXML
+    public Button Move4Button;
 
 
     private Trainer trainer;
@@ -60,6 +70,8 @@ public class FxmlBattleSceneController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Font.loadFont(getClass().getResourceAsStream("/fonts/pkmndp.ttf"), 14);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/pkmndpb.ttf"), 14);
 
         // Set battle background
         rand = r.nextInt(14)+1;
@@ -86,10 +98,38 @@ public class FxmlBattleSceneController implements Initializable {
         AnchorPane.setRightAnchor(MyPokemonStatusPanel, 40.0);
 
         MyPokemonName.setText(trainer.getPartyPokemons().get(0).getDisplayName());
+        AnchorPane.setTopAnchor(MyPokemonName, 15.0);
+        AnchorPane.setLeftAnchor(MyPokemonName, 10.0);
         MyPokemonHpText.setText(String.valueOf(trainer.getPartyPokemons().get(0).getStats().hp));
+        AnchorPane.setTopAnchor(MyPokemonHpText, 50.0);
+        AnchorPane.setLeftAnchor(MyPokemonHpText, 60.0);
 
         OpponentPokemonName.setText(opponent.getDisplayName());
+        AnchorPane.setTopAnchor(OpponentPokemonName, 15.0);
+        AnchorPane.setLeftAnchor(OpponentPokemonName, 10.0);
         OpponentPokemonHpText.setText(String.valueOf(opponent.getStats().hp));
+        AnchorPane.setTopAnchor(OpponentPokemonHpText, 50.0);
+        AnchorPane.setLeftAnchor(OpponentPokemonHpText, 60.0);
+
+        OpponentPokemonHpProgressBar.progressProperty().setValue(100);
+        MyPokemonHpProgressBar.progressProperty().setValue(100);
+        AnchorPane.setTopAnchor(OpponentPokemonHpProgressBar, 39.0);
+        AnchorPane.setLeftAnchor(OpponentPokemonHpProgressBar, 1.0);
+        AnchorPane.setTopAnchor(MyPokemonHpProgressBar, 39.0);
+        AnchorPane.setLeftAnchor(MyPokemonHpProgressBar, 1.0);
+
+        if(trainer.getPartyPokemons().get(0).getMoves().size() > 0) {
+            Move1Button.setText(trainer.getPartyPokemons().get(0).getMoves().get(0).getDisplayName());
+        }
+        if(trainer.getPartyPokemons().get(0).getMoves().size() > 1) {
+            Move2Button.setText(trainer.getPartyPokemons().get(0).getMoves().get(1).getDisplayName());
+        }
+        if(trainer.getPartyPokemons().get(0).getMoves().size() > 2) {
+            Move3Button.setText(trainer.getPartyPokemons().get(0).getMoves().get(2).getDisplayName());
+        }
+        if(trainer.getPartyPokemons().get(0).getMoves().size() > 3) {
+            Move4Button.setText(trainer.getPartyPokemons().get(0).getMoves().get(3).getDisplayName());
+        }
 
     }
 
